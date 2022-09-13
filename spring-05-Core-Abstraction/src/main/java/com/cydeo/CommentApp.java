62acd130766b73399862da52da6bd9d9a4df8d2b
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-13 21:14:20 +0200

    spring

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
@@ -14,6 +14,16 @@ public class CommentApp { // it should be under the main package
         comment.setAuthor("Johnson");
         comment.setText("Spring Framework");
 
+        /*
+        Comment is something that can change all the time.
+        Whenever user sends a new Comment, it is different than the others,
+        so object also has to be new.
+        But other objects we created beans from, are never changing objects,
+        their job is always the same, take something save it into database,
+        take something send it to UI etc. Those beans doesn't need to change.
+         */
+
+
 
         ApplicationContext container = new AnnotationConfigApplicationContext(CommentConfig.class);
 
