058ae8c8ec07bc514a9c041e2091e04dd748b5dd
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-26 21:59:36 +0200

    spring-thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,25 +1,54 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @RequestMapping("/register") // localhost:8080/mentor/register
-    public String register(){
+    @GetMapping("/register") //localhost:8080/mentor/register
+    public String register(Model model){
 
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
 
-        return "student/register";
+        model.addAttribute("mentor",new Mentor());
+
+        return "mentor/mentor-register";
     }
 
-    @RequestMapping("/drop") // localhost:8080/mentor/drop
-    public String drop(){
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+        //PathVariable is the part you capturing from URL itself.
+        //@ModelAttribute is the object you saved from the form in the view.
 
 
-        return "student/register";
+        //model.addAttribute("mentor",new Mentor()); // empty object
+
+        //return "mentor/mentor-register";
+        //return "mentor/mentor-confirmation";
+
+        return "redirect:/mentor/register";
     }
+
+
+    /*
+    @GetMapping("/confirm")
+    public String submitForm2(){
+        return "mentor/mentor-confirmation";
+    }
+
+     */
+
 
 
 }
--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,14 +3,14 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register") // localhost:8080/student/register
+    @RequestMapping(value = "/register",method = RequestMethod.GET) // localhost:8080/student/register
+    @GetMapping("/register")
     public String register(Model model){
 
         //                               attribute name which is here holding data(s)
@@ -31,7 +31,8 @@ public class StudentController {
 
 
 
-    @RequestMapping("/welcome") // localhost:8080/student/welcome?name=Ozzy  comes from register html
+   // @RequestMapping(value = "/welcome",method = RequestMethod.POST) // localhost:8080/student/welcome?name=Ozzy  comes from register html
+    @PostMapping("/welcome")
     public String welcome(@RequestParam String name){ // @RequestParam String name
 
         System.out.println(name);// you can also send this paramater to DataBase
