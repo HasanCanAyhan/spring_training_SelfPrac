ed6fcf558fa490fefbac3f3bb719702578b34d50
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-27 19:15:32 +0100

    spring-Rest-API

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -25,6 +25,32 @@ public class CourseController {
         return courseService.getCourses();
     }
 
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") Long courseId){
+
+        return courseService.getCourseById(courseId);
+    }
+
+    @GetMapping("category/{name}")
+    public List<CourseDTO>  getCourseByCategory(@PathVariable("name") String category){
+
+        return courseService.getCoursesByCategory(category);
+    }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") long courseId, @RequestBody CourseDTO course){
+        courseService.updateCourse(courseId,course);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourseById(@PathVariable("id") long courseId){
+        courseService.deleteCourseById(courseId);
+    }
 
 
 
