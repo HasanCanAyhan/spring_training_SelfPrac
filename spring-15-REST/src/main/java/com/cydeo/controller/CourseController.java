08d9836a1bb31432827f506178b92dca873c47f6
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-27 18:27:39 +0100

    spring-Rest-API

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,31 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@Controller
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    //@ResponseBody
+    public List<CourseDTO>  getAllCourses(){
+
+        //return all courses
+        return courseService.getCourses();
+    }
+
+
+
+
+}
