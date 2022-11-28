3fa56779f8dff45b1e3fcec82936e8e368be02e3
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-11-28 17:27:23 +0100

    spring-Rest-API

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -51,5 +51,18 @@ public class CourseController_ResponseEntity {
     }
 
 
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCourseById(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO courseDTO){
+        courseService.updateCourse(courseId,courseDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
